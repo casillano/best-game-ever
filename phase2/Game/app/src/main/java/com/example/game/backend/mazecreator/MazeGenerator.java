efd0208a9dcb742bc2975345a59f81414516d4b2
dom <dom.casillano@mail.utoronto.ca>
2019-11-29 18:08:28 -0500

    Moved drawing responsibilities from MazeGenerator to MazeScene to adhere to the Single Responsibility principle.

--- phase2/Game/app/src/main/java/com/example/game/backend/mazecreator/MazeGenerator.java
+++ phase2/Game/app/src/main/java/com/example/game/backend/mazecreator/MazeGenerator.java
@@ -19,176 +19,168 @@ import java.util.Stack;
 public class MazeGenerator {
 
 
-  private Cell[][] cells;
-  private static final int COLS = 3, ROWS = 4;
-  private Paint paint;
-  private int cellSize;
-  private Rect finishLine;
-  //keep track of all walls for collision detection
-  private ArrayList<Rect> walls;
-  private Random random;
+    private Cell[][] cells;
+    private static final int COLS = 3, ROWS = 4;
+    private Rect finishLine;
+    //keep track of all walls for collision detection
+    private ArrayList<Rect> walls;
+    private Random random;
 
-  public MazeGenerator() {
-    cells = new Cell[COLS][ROWS];
-    paint = new Paint();
-    paint.setColor(Color.BLACK);
-    paint.setStrokeWidth(3);
-    walls = new ArrayList<>();
-    random = new Random();
-    mazeGenerator();
-  }
-
-  public void drawMaze(Canvas canvas) {
-    int width = canvas.getWidth();
-    cellSize = width / (COLS + 1);
-    drawCells(canvas, cellSize);
-  }
-
-  private void drawCells(Canvas canvas, int cellSize) {
-
-
-    for (int x = 0; x < COLS; x++) {
-      for (int y = 0; y < ROWS; y++) {
-        if (x == COLS - 1 && y == ROWS - 1) {
-          Paint paint2 = new Paint();
-          paint2.setColor(Color.RED);
-          Rect finish = new Rect(x * cellSize + 220, y * cellSize + 520, (x + 1) * cellSize + 220, (y + 1) * cellSize + 520);
-          canvas.drawRect(finish, paint2);
-          finishLine = finish;
-        }
-        if (cells[x][y].topWall) {
-          Rect r = new Rect(x * cellSize + 220, y * cellSize + 520, (x + 1) * cellSize + 220, y * cellSize + 520 + 9);
-          canvas.drawRect(r, paint);
-          walls.add(r);
-        }
-        if (cells[x][y].botWall) {
-          Rect r = new Rect(x * cellSize + 220, (y + 1) * cellSize + 520, (x + 1) * cellSize + 220, (y + 1) * cellSize + 520 + 9);
-          canvas.drawRect(r, paint);
-          walls.add(r);
-        }
-        if (cells[x][y].rightWall) {
-          Rect r = new Rect((x + 1) * cellSize + 220, y * cellSize + 520, (x + 1) * cellSize + 220 + 9, (y + 1) * cellSize + 520);
-          canvas.drawRect(r, paint);
-          walls.add(r);
-        }
-        if (cells[x][y].leftWall) {
-          Rect r = new Rect(x * cellSize + 220, y * cellSize + 520, x * cellSize + 220 + 9, (y + 1) * cellSize + 520);
-          canvas.drawRect(r, paint);
-          walls.add(r);
-        }
-
-      }
-    }
-  }
-
-
-  private void mazeGenerator() {
-    Stack<Cell> stack = new Stack<>();
-    Cell current, next;
-
-    cells = new Cell[COLS][ROWS];
-
-    for (int x = 0; x < COLS; x++) {
-      for (int y = 0; y < ROWS; y++) {
-        cells[x][y] = new Cell(x, y);
-      }
+    public MazeGenerator() {
+        cells = new Cell[COLS][ROWS];
+        walls = new ArrayList<>();
+        random = new Random();
+        mazeGenerator();
     }
 
-    current = cells[0][0];
-    current.visited = true;
-    do {
-      next = getNeighbour(current);
-      if (next != null) {
-        removeWall(current, next);
-        stack.push(current);
-        current = next;
+    public ArrayList<Rect> getWalls() {
+        return this.walls;
+    }
+
+    public Rect getFinishLine() {
+        return this.finishLine;
+    }
+
+    public void setupMaze(Canvas canvas) {
+        int width = canvas.getWidth();
+        int cellSize = width / (COLS + 1);
+        createMazeCells(cellSize);
+    }
+
+    private void createMazeCells(int cellSize) {
+        for (int x = 0; x < COLS; x++) {
+            for (int y = 0; y < ROWS; y++) {
+                if (x == COLS - 1 && y == ROWS - 1) {
+                    finishLine = new Rect(x * cellSize + 220, y * cellSize + 520, (x + 1) * cellSize + 220, (y + 1) * cellSize + 520);
+                }
+                if (cells[x][y].topWall) {
+                    Rect r = new Rect(x * cellSize + 220, y * cellSize + 520, (x + 1) * cellSize + 220, y * cellSize + 520 + 9);
+                    walls.add(r);
+                }
+                if (cells[x][y].botWall) {
+                    Rect r = new Rect(x * cellSize + 220, (y + 1) * cellSize + 520, (x + 1) * cellSize + 220, (y + 1) * cellSize + 520 + 9);
+                    walls.add(r);
+                }
+                if (cells[x][y].rightWall) {
+                    Rect r = new Rect((x + 1) * cellSize + 220, y * cellSize + 520, (x + 1) * cellSize + 220 + 9, (y + 1) * cellSize + 520);
+                    walls.add(r);
+                }
+                if (cells[x][y].leftWall) {
+                    Rect r = new Rect(x * cellSize + 220, y * cellSize + 520, x * cellSize + 220 + 9, (y + 1) * cellSize + 520);
+                    walls.add(r);
+                }
+
+            }
+        }
+    }
+    private void mazeGenerator() {
+        Stack<Cell> stack = new Stack<>();
+        Cell current, next;
+
+        cells = new Cell[COLS][ROWS];
+
+        for (int x = 0; x < COLS; x++) {
+            for (int y = 0; y < ROWS; y++) {
+                cells[x][y] = new Cell(x, y);
+            }
+        }
+
+        current = cells[0][0];
         current.visited = true;
-      } else {
-        current = stack.pop();
-      }
-    } while (!stack.isEmpty());
-  }
-
-  private Cell getNeighbour(Cell cell) {
-
-    ArrayList<Cell> neighbours = new ArrayList<>();
-
-    //left neighbour
-    if (cell.col > 0) {
-      if (!cells[cell.col - 1][cell.row].visited) {
-        neighbours.add(cells[cell.col - 1][cell.row]);
-      }
-    }
-    //right neighbour
-    if (cell.col < COLS - 1) {
-      if (!cells[cell.col + 1][cell.row].visited) {
-        neighbours.add(cells[cell.col + 1][cell.row]);
-      }
+        do {
+            next = getNeighbour(current);
+            if (next != null) {
+                removeWall(current, next);
+                stack.push(current);
+                current = next;
+                current.visited = true;
+            } else {
+                current = stack.pop();
+            }
+        } while (!stack.isEmpty());
     }
 
-    //top neighbour
-    if (cell.row > 0) {
-      if (!cells[cell.col][cell.row - 1].visited) {
-        neighbours.add(cells[cell.col][cell.row - 1]);
-      }
+    private Cell getNeighbour(Cell cell) {
+
+        ArrayList<Cell> neighbours = new ArrayList<>();
+
+        //left neighbour
+        if (cell.col > 0) {
+            if (!cells[cell.col - 1][cell.row].visited) {
+                neighbours.add(cells[cell.col - 1][cell.row]);
+            }
+        }
+        //right neighbour
+        if (cell.col < COLS - 1) {
+            if (!cells[cell.col + 1][cell.row].visited) {
+                neighbours.add(cells[cell.col + 1][cell.row]);
+            }
+        }
+
+        //top neighbour
+        if (cell.row > 0) {
+            if (!cells[cell.col][cell.row - 1].visited) {
+                neighbours.add(cells[cell.col][cell.row - 1]);
+            }
+        }
+
+        //bottom neighbour
+        if (cell.row < ROWS - 1) {
+            if (!cells[cell.col][cell.row + 1].visited) {
+                neighbours.add(cells[cell.col][cell.row + 1]);
+            }
+        }
+        if (neighbours.size() > 0) {
+            int index = random.nextInt(neighbours.size());
+            System.out.println(index);
+            return neighbours.get(index);
+        }
+        return null;
     }
 
-    //bottom neighbour
-    if (cell.row < ROWS - 1) {
-      if (!cells[cell.col][cell.row + 1].visited) {
-        neighbours.add(cells[cell.col][cell.row + 1]);
-      }
+    private void removeWall(Cell current, Cell next) {
+        if (current.col == next.col && current.row == next.row + 1) {
+            current.topWall = false;
+            next.botWall = false;
+        }
+        if (current.col == next.col && current.row == next.row - 1) {
+            current.botWall = false;
+            next.topWall = false;
+        }
+        if (current.col == next.col + 1 && current.row == next.row) {
+            current.leftWall = false;
+            next.rightWall = false;
+        }
+        if (current.col == next.col - 1 && current.row == next.row) {
+            current.rightWall = false;
+            next.leftWall = false;
+        }
     }
-    if (neighbours.size() > 0) {
-      int index = random.nextInt(neighbours.size());
-      System.out.println(index);
-      return neighbours.get(index);
-    }
-    return null;
-  }
 
-  private void removeWall(Cell current, Cell next) {
-    if (current.col == next.col && current.row == next.row + 1) {
-      current.topWall = false;
-      next.botWall = false;
+    //TODO: move collision checking out of this class and into mazeScene
+    public boolean checkCollisions(Player player) {
+        if (walls.size() > 0) {
+            return CollisionChecker.checkCollisions(walls, player);
+        }
+        return false;
     }
-    if (current.col == next.col && current.row == next.row - 1) {
-      current.botWall = false;
-      next.topWall = false;
-    }
-    if (current.col == next.col + 1 && current.row == next.row) {
-      current.leftWall = false;
-      next.rightWall = false;
-    }
-    if (current.col == next.col - 1 && current.row == next.row) {
-      current.rightWall = false;
-      next.leftWall = false;
-    }
-  }
 
-  public boolean checkCollisions(Player player) {
-    if (walls.size() > 0) {
-      return CollisionChecker.checkCollisions(walls, player);
+    public boolean checkFinished(Player player) {
+        if (walls.size() > 0) {
+            return CollisionChecker.checkFinished(player, finishLine);
+        }
+        return false;
     }
-    return false;
-  }
-
-  public boolean checkFinished(Player player) {
-    if (walls.size() > 0) {
-      return CollisionChecker.checkFinished(player, finishLine);
-    }
-    return false;
-  }
 
 
-  private class Cell {
-    boolean topWall = true, leftWall = true, botWall = true, rightWall = true, visited = false;
-    int col, row;
+    private class Cell {
+        boolean topWall = true, leftWall = true, botWall = true, rightWall = true, visited = false;
+        int col, row;
 
 
-    Cell(int col, int row) {
-      this.col = col;
-      this.row = row;
+        Cell(int col, int row) {
+            this.col = col;
+            this.row = row;
+        }
     }
-  }
 }
--- phase2/Game/app/src/main/java/com/example/game/scenes/MazeScene.java
+++ phase2/Game/app/src/main/java/com/example/game/scenes/MazeScene.java
@@ -10,9 +10,10 @@ import android.view.MotionEvent;
 
 import com.example.game.design.Background;
 import com.example.game.design.Button;
-import com.example.game.backend.Constants;
 import com.example.game.backend.characters.player.Player;
 import com.example.game.backend.mazecreator.MazeGenerator;
+
+import java.util.ArrayList;
 
 
 /* Scene for a maze game
@@ -22,94 +23,109 @@ import com.example.game.backend.mazecreator.MazeGenerator;
 
 public class MazeScene implements Scene {
 
-  private Background background;
-  private Player player;
-  private Point playerPoint;
-  private Button quitButton;
-  private SceneManager manager;
-  private boolean gameOver = false, movingPlayer = false;
-  private MazeGenerator mazeGenerator;
-  private Rect r;
-  boolean firstDraw = true;
-  private int xp;
+    private Background background;
+    private Player player;
+    private Point playerPoint;
+    private Button quitButton;
+    private SceneManager manager;
+    private boolean gameOver = false, movingPlayer = false;
+    private MazeGenerator mazeGenerator;
+    private int xp;
+    private Paint wallPaint;
+    private Paint finishLinePaint;
 
-  MazeScene(Context context, SceneManager manager) {
-    this.manager = manager;
-    player = new Player(context, SceneManager.getCostume());
-    background = new Background(context);
-    player.changeRectangle(300, 550, 400, 650);
-    playerPoint = new Point(player.getRectangle().centerX(), player.getRectangle().centerY());
-    quitButton = new Button(850, 300, 100, 100, "X");
-    mazeGenerator = new MazeGenerator();
-    xp = 0;
-  }
-
-  @Override
-  public void update() {
-    background.update();
-    player.update(playerPoint);
-    //pop up showing loser, then return to main menu
-    if (mazeGenerator.checkCollisions(player)) {
-      gameOver = true;
-      terminate();
-      manager.resetScenes();
-
+    MazeScene(Context context, SceneManager manager) {
+        this.manager = manager;
+        wallPaint = new Paint();
+        finishLinePaint = new Paint();
+        wallPaint.setColor(Color.BLACK);
+        wallPaint.setStrokeWidth(3);
+        finishLinePaint.setColor(Color.RED);
+        player = new Player(context, SceneManager.getCostume());
+        background = new Background(context);
+        player.changeRectangle(300, 550, 400, 650);
+        playerPoint = new Point(player.getRectangle().centerX(), player.getRectangle().centerY());
+        quitButton = new Button(850, 300, 100, 100, "X");
+        mazeGenerator = new MazeGenerator();
+        xp = 0;
     }
-    //pop up showing winner
-    if (mazeGenerator.checkFinished(player)) {
-      xp = 150;
-      gameOver = true;
-      terminate();
-      manager.resetScenes();
-    }
-  }
 
-
-  @Override
-  public void draw(Canvas canvas) {
-    Paint paint = new Paint();
-    paint.setColor(Color.BLACK);
-    background.draw(canvas);
-    quitButton.draw(canvas);
-    mazeGenerator.drawMaze(canvas);
-    player.draw(canvas);
-
-  }
-
-  int getXp() {
-    return xp;
-  }
-
-  void setCostume(String color){
-    player.setCostume(color);
-  }
-
-  @Override
-  public void terminate() {
-    SceneManager.ACTIVE_SCENE = 1;
-  }
-
-  @Override
-  public void receiveTouch(MotionEvent event) {
-    switch (event.getAction()) {
-      case MotionEvent.ACTION_DOWN:
-        if (!gameOver && player.getRectangle().contains((int) event.getX(), (int) event.getY())) {
-          movingPlayer = true;
-          break;
+    private void drawMaze(Canvas canvas) {
+        mazeGenerator.setupMaze(canvas);
+        ArrayList<Rect> walls = mazeGenerator.getWalls();
+        Rect finishLine = mazeGenerator.getFinishLine();
+        for (int i = 0; i < walls.size(); i++) {
+            canvas.drawRect(walls.get(i), wallPaint);
         }
-      case MotionEvent.ACTION_MOVE:
-        if (!gameOver && movingPlayer) {
-          playerPoint.set((int) event.getX(), (int) event.getY());
-        }
-        break;
-      case MotionEvent.ACTION_UP:
-        movingPlayer = false;
-        break;
+        canvas.drawRect(finishLine, finishLinePaint);
+    }
 
+    @Override
+    public void update() {
+        background.update();
+        player.update(playerPoint);
+        //pop up showing loser, then return to main menu
+        if (mazeGenerator.checkCollisions(player)) {
+            gameOver = true;
+            terminate();
+            manager.resetScenes();
+
+        }
+        //pop up showing winner
+        if (mazeGenerator.checkFinished(player)) {
+            xp = 150;
+            gameOver = true;
+            terminate();
+            manager.resetScenes();
+        }
     }
-    if (quitButton.isClicked((int) event.getX(), (int) event.getY())) {
-      terminate();
-      manager.resetScenes();
+
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        background.draw(canvas);
+        quitButton.draw(canvas);
+        mazeGenerator.setupMaze(canvas);
+        player.draw(canvas);
+        drawMaze(canvas);
     }
-  }
+
+    int getXp() {
+        return xp;
+    }
+
+    void setCostume(String color) {
+        player.setCostume(color);
+    }
+
+    @Override
+    public void terminate() {
+        SceneManager.ACTIVE_SCENE = 1;
+    }
+
+    @Override
+    public void receiveTouch(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (!gameOver && player.getRectangle().contains((int) event.getX(), (int) event.getY())) {
+                    movingPlayer = true;
+                    break;
+                }
+            case MotionEvent.ACTION_MOVE:
+                if (!gameOver && movingPlayer) {
+                    playerPoint.set((int) event.getX(), (int) event.getY());
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                movingPlayer = false;
+                break;
+
+        }
+        if (quitButton.isClicked((int) event.getX(), (int) event.getY())) {
+            terminate();
+            manager.resetScenes();
+        }
+    }
 }
